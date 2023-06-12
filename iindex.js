0ba01d4ecec2c2657c5d0064786c95423402ffbb
asifh3741 <ah992612@gmail.com>
2023-06-12 04:07:45 -0700

    first commit

---
+++ iindex.js
@@ -0,0 +1,49 @@
+//My Family And My kit list.
+
+//My Father
+const father={
+    Name: "Azizul Islam",
+    Age:50,
+    Proffession: "germents employe",
+    Hobbies: ["singing","writing"],
+    Address:["Shardagonj","Kashimpur","Gazipur"]
+};
+
+//My Mother
+const mother={
+    Name:"Rosy begum",
+    Age:"39",
+    Proffession:["germents worker","house wife"],
+    Hobbies:["cooking","Abuse her words"],
+    Address:["shardagonj","kashimpur","gazipur"]
+}
+
+//My Sister
+const sister ={
+    Name: "Sumaiya",
+    Age:9,
+    Proffession: "student",
+    Hobby:"Drawing",
+    Address:"Amra jene thaki okhanei"
+};
+
+//My Phone
+const Phone={
+    Name:"huawey",
+    Ram:"3gb",
+    Rom:"32gb"
+}
+
+//My Laptop
+const laptop={
+    Device:"DESKTOP-I1DFOPO Microsoft surface laptop 3",
+    Processor:"Intel(R) Core(TM) i5-7300U CPU @ 2.60GHz 2.71 GHz",
+    InstalledRAM:"8.00 GB",
+    DeviceID:"E4002304-6593-48C8-92E3-1A36EBCBEF13",
+    ProductID:"00330-62989-50535-AAOEM",
+    SystemType:"64-bit operating system, x64-based processor",
+    PenAndTouch:"Pen and touch support with 10 touch points"
+};
+
+// I have no pet But i want to have a pet.
+
